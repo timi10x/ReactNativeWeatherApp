36157b2b498674f88d2426b455f9199dfb376cba
timmyCoder <olatoyedan@gmail.com>
2022-02-12 18:37:27 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.green}>
+        Open up App.js to start working on your app uuiuiu!
+        </Text>
+      <Text>This is a good way to start This</Text>
+      <TextInput style={{backgroundColor: 'red'}} placeholder="Password" />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  green: {
+    color: 'green'
+  }
 });
